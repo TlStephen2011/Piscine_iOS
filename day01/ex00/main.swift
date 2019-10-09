167826dbab9e515cfc1a1e23d46ca7e8d7a00e76
Tyrone Stephen <tlstephen11@gmail.com>
2019-10-09 22:51:34 +0200

    have to add ex04 for day01 and move playground to required files and folders

---
+++ day01/ex00/main.swift
@@ -0,0 +1,10 @@
+var c: Color = Color.spade
+print(c)
+
+var v: Value = Value.Jack
+print(v.rawValue)
+
+print(Value.init(rawValue: 13)!)
+
+print(Value.allValues)
+print(Color.allColors)
--- day01/temporaryPlayground/MyPlayground.playground/Contents.swift
+++ day01/temporaryPlayground/MyPlayground.playground/Contents.swift
@@ -46,8 +46,72 @@ class Card: NSObject {
         return "(\(self.value), \(self.color))"
     }
     
+    override func isEqual(_ obj: Any?) -> Bool {
+        if let obj = obj as? Card {
+            return color == obj.color && value == obj.value
+        } else {
+            return false
+        }
+    }
+}
+
+func ==(lhs: Card, rhs: Card) -> Bool {
+    return lhs.isEqual(rhs)
 }
 
 let card1 = Card(Color: Color.diamond, Value: Value.Ten)
+let card2 = Card(Color: Color.diamond, Value: Value.Ten)
+let card3 = Card(Color: Color.heart, Value: Value.King)
 
+print("IsEqual")
 print(card1)
+print(card1.isEqual(card2))
+print(card1.isEqual(card3))
+print()
+print("== operator")
+print(card1 == card2)
+print(card1 == card3)
+
+class Deck: NSObject {
+    static let allSpades:[Card] = Value.allValues.map({(val) -> Card in
+        return Card(Color: Color.spade, Value: val)
+    })
+    
+    static let allDiamonds:[Card] = Value.allValues.map({(val) -> Card in
+        return Card(Color: Color.diamond, Value: val)
+    })
+    
+    static let allHearts:[Card] = Value.allValues.map({(val) -> Card in
+        return Card(Color: Color.heart, Value: val)
+    })
+    
+    static let allClubs:[Card] = Value.allValues.map({(val) -> Card in
+        return Card(Color: Color.club, Value: val)
+    })
+    
+    static let allCards:[Card] = Deck.allHearts + Deck.allDiamonds + Deck.allSpades + Deck.allClubs
+}
+
+print()
+print(Deck.allSpades)
+print(Deck.allHearts)
+print()
+print("All Cards")
+print(Deck.allCards)
+
+extension Array {
+    mutating func shuffle() -> Void {
+        var swapPos = 0;
+        for i:Int in 0..<self.count {
+            swapPos = Int(arc4random_uniform(UInt32(self.count)))
+            self.swapAt(i, swapPos)
+        }
+    }
+}
+
+print()
+var cards = Deck.allSpades
+print("Shuffling....")
+cards.shuffle()
+print(cards)
+print()
