a106a7c16642bff06bafb9f081406dc30d750f8a
Tyrone Stephen <tlstephen11@gmail.com>
2019-10-11 12:47:28 +0200

    base death note added, changed deployment target to iOS 12.1 and added @available's to get it compatible with iOS 12.1

---
+++ day02/DeathNote/DeathNote/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DeathNote
+//
+//  Created by Tyrone STEPHEN on 2019/10/11.
+//  Copyright © 2019 Tyrone STEPHEN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
