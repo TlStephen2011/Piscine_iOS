d0ffc544264d73d5cfc65a6217945c482556cb6e
Tyrone Stephen <tlstephen11@gmail.com>
2019-10-14 11:28:10 +0200

    completed ex00

---
+++ day05/Kanto/Kanto/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Kanto
+//
+//  Created by Tyrone STEPHEN on 2019/10/14.
+//  Copyright © 2019 Tyrone Stephen. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ day05/Kanto/Kanto/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Kanto
+//
+//  Created by Tyrone STEPHEN on 2019/10/14.
+//  Copyright © 2019 Tyrone Stephen. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
